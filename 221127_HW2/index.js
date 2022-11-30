5e85abb140111e26758e4ff7ba7e7082de39cf53
Ya-Rong <happycayla@gmail.com>
2022-11-30 11:33:27 +0800

    HW2

---
+++ 221127_HW2/index.js
@@ -0,0 +1,93 @@
+const resultE1 = document.getElementById('result');
+let resultStr = '';
+
+const button1 = document.getElementById('1');
+button1.addEventListener('click', handleClick1);
+function handleClick1 (MouseEvent) {
+    resultStr += '1';
+    resultE1.innerText = resultStr;
+};
+
+const button2 = document.getElementById('2');
+button2.addEventListener('click', handleClick2);
+function handleClick2 (MouseEvent) {
+    resultStr += '2';
+    resultE1.innerText = resultStr;
+};
+
+const button3 = document.getElementById('3');
+button3.addEventListener('click', handleClick3);
+function handleClick3 (MouseEvent) {
+    resultStr += '3';
+    resultE1.innerText = resultStr;
+};
+
+const button4 = document.getElementById('4');
+button4.addEventListener('click', handleClick4);
+function handleClick4 (MouseEvent) {
+    resultStr += '4';
+    resultE1.innerText = resultStr;
+};
+
+const button5 = document.getElementById('5');
+button5.addEventListener('click', handleClick5);
+function handleClick5 (MouseEvent) {
+    resultStr += '5';
+    resultE1.innerText = resultStr;
+};
+
+const button6 = document.getElementById('6');
+button6.addEventListener('click', handleClick6);
+function handleClick6 (MouseEvent) {
+    resultStr += '6';
+    resultE1.innerText = resultStr;
+};
+
+const button7 = document.getElementById('7');
+button7.addEventListener('click', handleClick7);
+function handleClick7 (MouseEvent) {
+    resultStr += '7';
+    resultE1.innerText = resultStr;
+};
+
+const button8 = document.getElementById('8');
+button8.addEventListener('click', handleClick8);
+function handleClick8 (MouseEvent) {
+    resultStr += '8';
+    resultE1.innerText = resultStr;
+};
+
+const button9 = document.getElementById('9');
+button9.addEventListener('click', handleClick9);
+function handleClick9 (MouseEvent) {
+    resultStr += '9';
+    resultE1.innerText = resultStr;
+};
+
+const button0 = document.getElementById('0');
+button0.addEventListener('click', handleClick0);
+function handleClick0 (MouseEvent) {
+    resultStr += '0';
+    resultE1.innerText = resultStr;
+};
+
+
+
+
+// // console.log('hello world');
+// // function test () {
+// //     console.log('hello world');
+// // }
+
+// const buttonE1 = document.getElementById('test');
+// buttonE1.addEventListener('click',handleClick);
+// function handleClick (MouseEvent) {
+//     console.log(MouseEvent);
+// };
+
+// // console.log(buttonE1);
+// // buttonE1.onclick = test;
+
+// // function test () {
+
+// // }
