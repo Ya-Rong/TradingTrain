5ba0998f636000b51d78d05a46c77fea54c72ef3
Ya-Rong <happycayla@gmail.com>
2022-12-05 14:17:22 +0800

    HW2

--- 221127_HW2/try3/index3.js
+++ 221127_HW2/try3/index3.js
@@ -1,20 +1,20 @@
-// This function clear all the values
+//  clear 
 function clearScreen() {
-    document.getElementById("result").value = "";
-    }
+document.getElementById("result").value = "";
+}
     
-    // This function display values
-    function display(value) {
-    document.getElementById("result").value += value;
-    }
+// display
+function display(value) {
+document.getElementById("result").value += value;
+}
     
-    // This function evaluates the expression and returns result
-    function calculate() {
-    var p = document.getElementById("result").value;
-    var q = eval(p);
-    document.getElementById("result").value = q;
-    }
-    function backspace() {
-        var value = document.getElementById("result").value;
-        document.getElementById("result").value = value.substr(0, value.length - 1);
-    }
+// evaluates the expression and returns result
+function calculate() {
+var p = document.getElementById("result").value;
+var q = eval(p);
+document.getElementById("result").value = q;
+}
+function backspace() {
+var value = document.getElementById("result").value;
+document.getElementById("result").value = value.substr(0, value.length - 1);
+}
