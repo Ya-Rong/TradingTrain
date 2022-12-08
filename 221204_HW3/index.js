ef8384755ca80f04d3dd94812e66ce9cfd07a1c3
Ya-Rong <happycayla@gmail.com>
2022-12-08 20:01:20 +0800

    HW3

--- 221204_HW3/index.js
+++ 221204_HW3/index.js
@@ -2,7 +2,7 @@ const nickNameE1 = document.getElementById('nickName');
 const emailE1 = document.getElementById('email');
 const phoneE1 = document.getElementById('phone');
 const addButton = document.getElementById('addButton');
-const deleteButton = document.getElementById('deleteButton');
+// const deleteButton = document.getElementById('deleteButton');
 const tbody = document.getElementById('tbody');
 addButton.addEventListener('click', addNewRow);
 
@@ -11,16 +11,16 @@ function addNewRow () {
     const email = emailE1.value;
     const phone = phoneE1.value;
     // console.log((nickName, email, phone ));
-    const tr document.createElement('tr');
-    const td1 document.createElement('td');
-    const td2 document.createElement('td');
-    const td3 document.createElement('td');
-    const td4 document.createElement('td');
-    const td5 document.createElement('td');
+    const tr = document.createElement('tr');
+    const td1 = document.createElement('td');
+    const td2 = document.createElement('td');
+    const td3 = document.createElement('td');
+    const td4 = document.createElement('td');
+    const td5 = document.createElement('td');
     td1.innerText = nickName;
     td2.innerText = email;
     td3.innerText = phone;
-    td4.innerText = new Data().toISOString();
+    td4.innerText = new Date(); //目前這樣可以直接顯示台北標準時間(加後面這串時間會出錯) .toISOString()
     td5.innerHTML = '<button>修改</button><button>刪除</button>';
     tr.appendChild(td1);
     tr.appendChild(td2);
@@ -28,14 +28,22 @@ function addNewRow () {
     tr.appendChild(td4);
     tr.appendChild(td5);
     tbody.appendChild(tr);
-
 }
 
+// 這邊後面還沒寫完，還需要改
 deleteButton.remove();
 
-// function deleteRow () {
-
-// }
+function deleteRow () {
+    const nickName = nickNameE1.value;
+    const email = emailE1.value;
+    const phone = phoneE1.value;
+    const tr = document.getElement('tr');
+    const td1 = document.getElement('td');
+    const td2 = document.getElement('td');
+    const td3 = document.getElement('td');
+    const td4 = document.getElement('td');
+    const td5 = document.getElement('td');
+}
 
 // const members = [{
 //     name: '123',
@@ -45,7 +53,5 @@ deleteButton.remove();
 // },{
 
 // }]
-
-
 
 console.log(nickNameE1.value)
