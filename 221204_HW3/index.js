6f66342488de7b637dc1562702edf1aeda40039d
Ya-Rong <happycayla@gmail.com>
2022-12-11 11:29:18 +0800

    HW3

--- 221204_HW3/index.js
+++ 221204_HW3/index.js
@@ -2,9 +2,10 @@ const nickNameE1 = document.getElementById('nickName');
 const emailE1 = document.getElementById('email');
 const phoneE1 = document.getElementById('phone');
 const addButton = document.getElementById('addButton');
-// const deleteButton = document.getElementById('deleteButton');
+const deleteButton = document.getElementById('deleteButton');
 const tbody = document.getElementById('tbody');
 addButton.addEventListener('click', addNewRow);
+deleteButton.addEventListener('click', deleteRow);
 
 function addNewRow () {
     const nickName = nickNameE1.value;
@@ -20,7 +21,7 @@ function addNewRow () {
     td1.innerText = nickName;
     td2.innerText = email;
     td3.innerText = phone;
-    td4.innerText = new Date(); //目前這樣可以直接顯示台北標準時間(加後面這串時間會出錯) .toISOString()
+    td4.innerText = new Date().toLocaleString(); //目前這樣可以直接顯示台北標準時間(加後面這串時間會出錯) .toISOString()
     td5.innerHTML = '<button>修改</button><button>刪除</button>';
     tr.appendChild(td1);
     tr.appendChild(td2);
@@ -31,18 +32,23 @@ function addNewRow () {
 }
 
 // 這邊後面還沒寫完，還需要改
-deleteButton.remove();
+
 
 function deleteRow () {
-    const nickName = nickNameE1.value;
-    const email = emailE1.value;
-    const phone = phoneE1.value;
-    const tr = document.getElement('tr');
-    const td1 = document.getElement('td');
-    const td2 = document.getElement('td');
-    const td3 = document.getElement('td');
-    const td4 = document.getElement('td');
-    const td5 = document.getElement('td');
+    const tr = document.getElementById('tr');
+    const td1 = document.getElementById('td');
+    const td2 = document.getElementById('td');
+    const td3 = document.getElementById('td');
+    const td4 = document.getElementById('td');
+    const td5 = document.getElementById('td');
+    tr.parentNode.removeChild(tr);
+    td1.parentNode.removeChild(td);
+    td2.parentNode.removeChild(td);
+    td3.parentNode.removeChild(td);
+    td4.parentNode.removeChild(td);
+    td5.parentNode.removeChild(td);
+    // const tbody = document.getElementById(tbody);
+    // tbody.deleteCell();
 }
 
 // const members = [{
@@ -54,4 +60,4 @@ function deleteRow () {
 
 // }]
 
-console.log(nickNameE1.value)
+
