aade533125735220e56946f6e9affceeb76dc9d3
Ya-Rong <happycayla@gmail.com>
2022-12-11 17:34:47 +0800

    HW3

--- 221204_HW3/index.js
+++ 221204_HW3/index.js
@@ -1,11 +1,9 @@
 const nickNameE1 = document.getElementById('nickName');
 const emailE1 = document.getElementById('email');
 const phoneE1 = document.getElementById('phone');
-const addButton = document.getElementById('addButton');
 const tbody = document.getElementById('tbody');
+const addButton = document.getElementById('addButton');
 addButton.addEventListener('click', addNewRow);
-
-
 function addNewRow () {
     const nickName = nickNameE1.value;
     const email = emailE1.value;
@@ -20,7 +18,7 @@ function addNewRow () {
     td2.innerText = email;
     td3.innerText = phone;
     td4.innerText = new Date().toLocaleString();
-    td5.innerHTML = '<button>修改</button><button>刪除</button>';
+    td5.innerHTML = '<button>修改</button><input type="button" value="刪除" id="delete" onclick="deleteRow(this)"></input>'
     tr.appendChild(td1);
     tr.appendChild(td2);
     tr.appendChild(td3);
@@ -29,8 +27,8 @@ function addNewRow () {
     tbody.appendChild(tr);
 }
 
-// 這邊後面還沒寫完，還需要改
-const deleteButton = document.getElementById('deleteButton');
+// 後面還需要改
+const deleteButton = document.getElementById('delete');
 deleteButton.addEventListener('click', deleteRow);
 function deleteRow () {
     const tr = document.getElementById('tr');
@@ -39,14 +37,16 @@ function deleteRow () {
     const td3 = document.getElementById('td');
     const td4 = document.getElementById('td');
     const td5 = document.getElementById('td');
-    Node.removeChild(tr);
-    Node.removeChild(td1);
-    Node.removeChild(td2);
-    Node.removeChild(td3);
-    Node.removeChild(td4);
-    Node.removeChild(td5);
+    tr.removeChild(tr);
+    td1.removeChild(td1);
+    td2.removeChild(td2);
+    td3.removeChild(td3);
+    td4.removeChild(td4);
+    td5.removeChild(td5);
 }
-
+// if (Node.parentNode) {
+//     Node.parentNode.removeChild(Node);
+// }
 // const members = [{
 //     name: '123',
 //     email: '123@gmail.com',
--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 #### calculator
 ##### [HW2_fail](https://ya-rong.github.io/TradingTrain/221127_HW2/index.html)
 ##### [HW2](https://ya-rong.github.io//TradingTrain/221127_HW2/try3/index3.html)
+#### member management
+##### [HW3_fail](https://ya-rong.github.io//TradingTrain/221204_HW3/index.html)
+
